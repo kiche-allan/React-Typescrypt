4d325f8e1615416edbb952ab16031c2fcb274109
allan Kiche <allankiche18@gmail.com>
2022-10-29 18:59:16 +0300

    made change in greet component

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -3,7 +3,7 @@ import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from 
 export const Greet =(props: { name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined }) => {
     return (
         <div>
-            <h2>Welcome {props.name}, Youve got an urgent call</h2>
+            <h2>Welcome in {props.name}, Youve got an urgent call</h2>
         </div>
     )
 }
