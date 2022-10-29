802d97b12f5e8e3eff1b8b46b69c9e9097d7939a
allan Kiche <allankiche18@gmail.com>
2022-10-29 18:56:10 +0300

    first commit

---
+++ src/components/Greet.tsx
@@ -0,0 +1,9 @@
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react"
+
+export const Greet =(props: { name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined }) => {
+    return (
+        <div>
+            <h2>Welcome {props.name}, Youve got an urgent call</h2>
+        </div>
+    )
+}
