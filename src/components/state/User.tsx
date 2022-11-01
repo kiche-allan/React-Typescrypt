7202a69d42ba8340cef782422daf694ec1fecedb
Allan Kiche <tle2819@gmail.com>
2022-11-02 01:34:47 +0300

    usestate type asserions

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,23 +6,23 @@ type AuthUser = {
 }
 
 export const User = () => {
-    const [user, setUser] = useState<AuthUser | null>(null)
+    const [user, setUser] = useState<AuthUser>({}  as AuthUser)
     const handleLogin = () => {
         setUser({
             name: 'John',
             email: 'allankiche18@gmail.com'
         })
     }
-    const handleLogout = () => {
-        setUser(null)
-    }
+    // const handleLogout = () => {
+    //     setUser(AuthUser)
+    // }
 
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
-            <div>User name is {user?.name}</div>
-            <div>User email is {user?.name}</div>
+            {/* <button onClick={handleLogout}>Logout</button> */}
+            <div>User name is {user.name}</div>
+            <div>User email is {user.name}</div>
         </div>
     )
 }
