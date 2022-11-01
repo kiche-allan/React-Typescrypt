5ec59a523414132f41dcfd4432bf810be2fb3526
Allan Kiche <tle2819@gmail.com>
2022-11-02 01:29:15 +0300

    usestate hook

---
+++ src/components/state/User.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+type AuthUser = {
+    name: string
+    email: string
+}
+
+export const User = () => {
+    const [user, setUser] = useState<AuthUser | null>(null)
+    const handleLogin = () => {
+        setUser({
+            name: 'John',
+            email: 'allankiche18@gmail.com'
+        })
+    }
+    const handleLogout = () => {
+        setUser(null)
+    }
+
+    return (
+        <div>
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogout}>Logout</button>
+            <div>User name is {user?.name}</div>
+            <div>User email is {user?.name}</div>
+        </div>
+    )
+}
