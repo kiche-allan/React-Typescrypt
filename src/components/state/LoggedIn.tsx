cab4fb7cecf7ead7129f973d89c73815706a33ea
Allan Kiche <tle2819@gmail.com>
2022-11-02 00:52:33 +0300

    added use state

---
+++ src/components/state/LoggedIn.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export const isLoggedIn =  () => {
+   
+//create a login state variable
+const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    const handleLogin = () => {
+        setIsLoggedIn(true)
+    }
+    const handleLogout = () => {
+        setIsLoggedIn(false)
+    }
+    return (
+        <><><button onClick={handleLogin}>Login</button><button onClick={handleLogout}>Logout</button><div>User is Logged In/ Logged Out</div></><div>User is {isLoggedIn ? 'logged in' : 'logged out'}</div></>
+    )
+}
